8c1fe67d2fc4fd0ada7b42bfc308bebcdc4e2125
Alberto <47937297+albertopd@users.noreply.github.com>
2025-06-20 10:22:51 +0200

    Ops, code wasn't running yet

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from data_cleanner import DataCleanner
 
 data_cleaner = DataCleanner("data/properties.csv")
 data_cleaner.clean_duplicates()
-data_cleaner.clean_blank_spaces()
 data_cleaner.clean_errors()
+data_cleaner.clean_empty_cells()
 
